bbbb4ec689f84060ec78a95618c22ade47d999a2
Batuhan <pusbatuhan@gmail.com>
2022-02-19 10:25:42 +0300

    Güncelleme

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-var add = document.querySelector()
+var add = document.querySelector("#task");
+var button = document.querySelector("#liveToastBtn");
+var list = document.querySelector("#list");
+let itemsArray = localStorage.getItem('kisiler') ? JSON.parse(localStorage.getItem('kisiler')) : [];
+let element = document.createElement("li");
+
+function newElement() {
+  if (add.value.trim() == "") {
+    add.value = "";
+  } else {
+    itemsArray.unshift(add.value);
+    localStorage.setItem('kisiler', JSON.stringify(itemsArray));
+    let data = JSON.parse(localStorage.getItem('kisiler'));
+    console.log(data)
+    add.value = "";
+  }
+  
+}
+
+console.log(data)
